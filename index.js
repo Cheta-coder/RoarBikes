16949e30a62c56731319b3d68ae4b2837f5b323f
Cheta-coder <happinessugwuanyi7@gmail.com>
2023-10-20 15:50:12 +0100

    Just finished with the mobile view and tablet

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let mobileNav = document.querySelector('.mobileNav');
 let hamburger = document.querySelector('.hamburger');
 let closeIcon = document.querySelector('.closeIcon');
+let slideIndex = 1;
+// showSlides(slideIndex);
 
 mobileNav.style.visibility = 'hidden'
 
@@ -13,5 +15,42 @@ hamburger.addEventListener('click', ()=>{
 
 closeIcon.addEventListener('click', ()=>{
     mobileNav.style.visibility ='hidden'
-    hamburger.style.visibility = 'visible'
-})
+    hamburger.style.visibility = 'visible';
+});
+
+// Next or previous controls
+let plusSlides = (n)=> {
+    showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+let currentSlide = (n) => {
+    showSlides(slideIndex = n);
+}
+
+// let showSlides = (n)=> {
+//     let i;
+//     let slides = document.querySelectorAll('.mySlide')
+//     let dots = document.querySelectorAll('.dot')
+//     if (n > slides.length){slideIndex =1}
+//     if (n < 1){slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++){
+//         dots[i].className = dots[i].className.replace("active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//     dots[slideIndex-1].className += "active";
+// }
+
+// let showSlides = ()=>{
+//     let i;
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length){slideIndex = 1}
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showSlides, 2000)
+//     // Change image every 2 seconds
+// }
